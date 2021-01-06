b8246def075028f9f9d8b65a76cf9cda6600515f
RachelleAnnAgapor <73839967+RachelleAnnAgapor@users.noreply.github.com>
2021-01-06 15:47:53 +0800

    Add files via upload

---
+++ views.py
@@ -0,0 +1,27 @@
+from django.shortcuts import render
+from .models import *
+
+# Create your views here.
+def index(request):
+    return render(request, 'index.html')
+
+def display_laptops(request):
+    items = Laptop.objects.all()
+    context = {
+    'items' : items,
+    }
+    return render(request, 'index.html', context) # 3 arguments
+
+def display_desktops(request):
+    items = Desktop.objects.all()
+    context = {
+    'items' : items,
+    }
+    return render(request, 'index.html', context)
+
+def display_mobiles(request):
+    items = Mobile.objects.all()
+    context = {
+    'items' : items,
+    }
+    return render(request, 'index.html', context)
